650fb07dcbefbe3bab8a7d872c69ad9eaffefa58
Samsoucy <newf930@outlook.com>
2019-02-27 18:32:00 -0500

    added students table

--- migrations/20190227174029_cohorts.js
+++ migrations/20190227174029_cohorts.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
         tbl.increments();
 
         tbl
-            .string("name")
+            .text("name")
             .notNullable()
             .unique()
         
